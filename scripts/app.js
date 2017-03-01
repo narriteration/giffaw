42e99ea1fa8caadba69b800a98c9acb27a84a8cd
narriteration <narriteration@gmail.com>
2017-02-28 18:35:22 -0800

    added onSuccess function operations

--- scripts/app.js
+++ scripts/app.js
@@ -3,8 +3,8 @@ $(document).on("ready", function(){
     $('form').on('submit', function getGifImages(e) {
         e.preventDefault();
 
-        var userData = ;
-        var endpoint = ;
+        var userData = $('form').serialize();
+        var endpoint = 'http://api.giphy.com/v1/gifs/search';
 
         $.ajax ({
             method: 'GET',
@@ -13,16 +13,19 @@ $(document).on("ready", function(){
             dataType: 'json',
             success: onSuccess,
             error: onError,
-
         });
-
-
     });
 
 function onSuccess(successJson) {
     console.log(successJson);
-    //
-}
+    successJson.data.forEach(function(el){
+        var isUrl = el.images.fixed_height.url;
+        var images = new Image();
+            images.src = isUrl;
+            $(".gif-gallery").append(images);
+    });
+// incorrect:    successJson.forEach(data.images.fixed_height.url);
+};
 
 function onError(errorJson, status, errorThrown) {
     console.log(errorThrown)
