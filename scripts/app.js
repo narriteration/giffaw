e99c37e342a4f29e0df71053aadae37c23234cae
narriteration <narriteration@gmail.com>
2017-02-28 17:37:57 -0800

    created basic structure of app.js file

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,34 @@
 $(document).on("ready", function(){
 
+    $('form').on('submit', function getGifImages(e) {
+        e.preventDefault();
+
+        var userData = ;
+        var endpoint = ;
+
+        $.ajax ({
+            method: 'GET',
+            url: endpoint,
+            data: userData,
+            dataType: 'json',
+            success: onSuccess,
+            error: onError,
+
+        });
+
+
+    });
+
+function onSuccess(successJson) {
+    console.log(successJson);
+    //
+}
+
+function onError(errorJson, status, errorThrown) {
+    console.log(errorThrown)
+}
+
+
+
+
 });
